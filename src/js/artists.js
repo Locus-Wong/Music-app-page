74c58c8b86ed0c1c34e1ab98b781a210f6afbadd
TSZ WA WONG <twwong9@myseneca.ca>
2024-06-09 20:11:28 -0400

    Added html, css, js

---
+++ src/js/artists.js
@@ -0,0 +1,36 @@
+window.artists = [
+  {
+    artistId: "AID-64700",
+    name: "Taylor Swift",
+    urls: [
+      { url: "https://en.wikipedia.org/wiki/Taylor_Swift”, name: “Wikipedia" },
+      { url: "https://www.instagram.com/taylorswift/”, name: “Instagram" },
+    ],
+  },
+  {
+    artistId: "AID-30751",
+    name: "Bruno Mars",
+    urls: [
+      { url: "https://en.wikipedia.org/wiki/Bruno_Mars”, name: “Wikipedia" },
+      { url: "https://www.instagram.com/brunomars/?hl=en”, name: “Instagram" },
+    ],
+  },
+  {
+    artistId: "AID-92377",
+    name: "Ed Sheeran",
+    urls: [
+      { url: "https://en.wikipedia.org/wiki/Ed_Sheeran”, name: “Wikipedia" },
+      { url: "https://www.instagram.com/teddysphotos/”, name: “Instagram" },
+    ],
+  },
+  {
+    artistId: "AID-37343",
+    name: "Shawn Mendes",
+    urls: [
+      { url: "https://en.wikipedia.org/wiki/Shawn_Mendes”, name: “Wikipedia" },
+      {
+        url: "https://www.instagram.com/shawnmendes/?hl=en”, name: “Instagram",
+      },
+    ],
+  },
+];
